ba8b9db3034d61aa54969dc6d960bb59e45529ba
KDDilshan <kdprojects847@gmail.com>
2024-11-07 14:04:04 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SignUp from './signUp'
+import Login from './Login'
+import Home from './Home'
 
 function App() {
   return (
-        <div>
-          
-        </div>
+       <BrowserRouter>
+        <Routes>
+            <Route path='/register' element={<SignUp />}/>
+            <Route path='/login' element={<Login />} />
+            <Route path='/home' element={<Home />} />
+        </Routes>
+       </BrowserRouter>
   )
 }
 
