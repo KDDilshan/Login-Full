033f57ae392db0cafbfa5ce11e05f44c086cc664
KDDilshan <kdprojects847@gmail.com>
2024-11-08 01:36:08 +0530

    completion of project

---
+++ Api/index.js
@@ -0,0 +1,36 @@
+const expres=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+const EmployeeModel=require('./models/Employee')
+
+const app=expres()
+app.use(expres.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://kdprojects847:KDdJKOyXQAdD6Kxd@cluster1.7jyzc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
+
+app.post("/login",(req,res)=>{
+    const {email,password}=req.body
+    EmployeeModel.findOne({email:email})
+    .then(user=>{
+        if(user){
+            if(user.password===password){
+                res.json("Success")
+            }else{
+                res.json("The password is incorrect")
+            }
+        }else{
+            res.json("no record is existed")
+        }
+    })
+})
+
+app.post("/register",(req,res)=>{
+    EmployeeModel.create(req.body)
+    .then(employees=>res.json(employees))
+    .catch(err=>res.json(err))
+})
+
+app.listen(3001,()=>{
+    console.log("server is runing")
+})
--- Api/models/Employee.js
+++ Api/models/Employee.js
@@ -2,10 +2,11 @@ const mongoose=require('mongoose')
 
 const employeeShema=new mongoose.Schema({
     name:String,
-    emaiil:String,
+    email:String,
     password:String
 })
 
 const EmployeeModel=mongoose.model("employees",employeeShema)
 
 module.exports=EmployeeModel
+
